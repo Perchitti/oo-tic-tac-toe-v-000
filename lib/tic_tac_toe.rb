cbb2ee569f624bf6137b36f36b2442e718767bb7
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-01 17:52:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,7 +19,6 @@ end
   def play
     while !over?
       turn
-    end
     if won?
       puts "Congratulations #{winner}!"
     elsif draw?
@@ -96,7 +95,7 @@ end
   end
 
   def winner
-    if winning_combo = won?(@board)
+    if winning_combo = won?
       @board[winning_combo.first]
     end
   end
