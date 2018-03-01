75ed070bed67508ae5e4dc45b47b715e127c5ec5
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-01 17:40:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,12 +68,13 @@ end
     puts "Please enter 1-9:"
     user_input = gets.strip
     index = input_to_index(user_input)
-    if valid_move?(@board, index)
-      move(@board, index, current_player(@board))
+    if valid_move? (index)
+      move(index, current_player(@board))
       display_board(board)
     else
       turn(@board)
     end
+    display_board
   end
 
   def position_taken?(index)
